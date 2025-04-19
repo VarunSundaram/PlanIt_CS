a1049de1b1ee84e585ed245f90651b404d49357d
Varun kumar Sundaram <varunkumarsundaram@Varuns-MacBook-Pro.local>
2025-04-19 20:57:34 +0930

    restructured solution

---
+++ PlanIT_Func/PlanIT_Trigger.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace PlanIt.Task
+{
+    public class PlanIT_Trigger
+    {
+        private readonly ILogger<PlanIT_Trigger> _logger;
+
+        public PlanIT_Trigger(ILogger<PlanIT_Trigger> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("PlanIT_Trigger")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+Jupiter Test Demo
+
+This Project is an Azure function, httptrigger, which will run on development.
+
+Program Description: The thread runs on demand after each deployment. This contains an automated solution to launcha and executes tests on JupiterTesting.com. The test suite is created using NUnit framework. 
+
+Build and Deploy Pipeline: Using Github delivery protocols, automated merge into the cloud is triggered. Solution is deployed into Azure host for Functions.
+
+Tools and Programming language C#, selenium, azure, vscode and Nunit .Net interpreter is used to develop the solution
